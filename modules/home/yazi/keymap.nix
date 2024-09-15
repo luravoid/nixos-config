bd5a126759f6e91680122e250259fbc70be429a6
luravoid <example@example.com>
2024-09-15 16:23:48 +0200

    add keys to yazi conf

--- modules/home/yazi/keymap.nix
+++ modules/home/yazi/keymap.nix
@@ -10,6 +10,54 @@
         run = "shell --confirm 'sig -o'";
         desc = "Preview all images in the current directory";
       }
+      { on = ["g" "h"];
+        run = "cd ~";
+        desc = "Go to the home directory";
+      }
+      { on = ["g" "n"];
+        run = "cd nixos-config";
+        desc = "Go to the home directory";
+      }
+      {
+        on = ["g" "c"];
+        run = "cd ~/.config";
+        desc = "Go to the config directory";
+      }
+      {
+        on = ["g" "d"];
+        run = "cd ~/Downloads";
+        desc = "Go to the downloads directory";
+      }
+      {
+        on = ["g" "D"];
+        run = "cd ~/Documents";
+        desc = "Go to the documents directory";
+      }
+      {
+        on = ["g" "p"];
+        run = "cd ~/Pictures";
+        desc = "Go to the pictures directory";
+      }
+      {
+        on = ["g" "v"];
+        run = "cd ~/Videos";
+        desc = "Go to the videos directory";
+      }
+      {
+        on = ["g" "w"];
+        run = "cd ~/Pictures/Backgrounds";
+        desc = "Go to the wallpapers directory";
+      }
+      {
+        on = ["g" "t"];
+        run = "cd /tmp";
+        desc = "Go to the temporary directory";
+      }
+      {
+        on = ["g" "<Space>"];
+        run = "cd --interactive";
+        desc = "Go to a directory interactively";
+      }
     ];
   };
 }
