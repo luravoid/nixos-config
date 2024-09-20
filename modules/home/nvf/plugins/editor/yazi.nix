dc3b0a2e15b70ada02f35138a6d232ceebe166fe
luravoid <example@example.com>
2024-09-20 11:49:31 +0200

    nvf - add yazi binding

--- modules/home/nvf/plugins/editor/yazi.nix
+++ modules/home/nvf/plugins/editor/yazi.nix
@@ -3,6 +3,11 @@
     extraPlugins = with pkgs.vimPlugins; {
       yazi-nvim = {
         package = yazi-nvim;
+        setup = "vim.keymap.set('n', '<leader>cy', function()
+          require('yazi').yazi()
+         end)";
+        # https://github.com/mikavilpas/yazi.nvim/issues/340
+        # "require('yazi').setup()";
       };
     };
   };
