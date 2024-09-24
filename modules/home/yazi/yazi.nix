aab1cbb7f729ca7db958966528fb7d9e9fc07287
luravoid <example@example.com>
2024-09-24 21:09:25 +0200

    edit and format yazi conf

--- modules/home/yazi/yazi.nix
+++ modules/home/yazi/yazi.nix
@@ -1,11 +1,14 @@
-{ pkgs, config, ... }:
 {
+  pkgs,
+  config,
+  ...
+}: {
   programs.yazi = {
     enable = true;
 
     settings = {
       manager = {
-        ratio = [ 2 3 3 ];
+        ratio = [2 3 3];
         sort_by = "natural";
         sort_sensitive = false;
         sort_reverse = false;
@@ -15,7 +18,7 @@
         show_hidden = true;
         show_symlink = true;
         scrolloff = 5;
-        mouse_events = [ "click" "scroll" ];
+        mouse_events = ["click" "scroll"];
         title_format = "Yazi: {cwd}";
       };
 
@@ -28,42 +31,102 @@
         image_quality = 75;
         sixel_fraction = 15;
         ueberzug_scale = 1;
-        ueberzug_offset = [ 0 0 0 0 ];
+        ueberzug_offset = [0 0 0 0];
       };
 
       opener = {
         edit = [
-          { run = "nvim \"$@\""; desc = "$EDITOR"; block = true; for = "unix"; }
+          {
+            run = "nvim \"$@\"";
+            desc = "$EDITOR";
+            block = true;
+            for = "unix";
+          }
         ];
         open = [
-          { run = "xdg-open \"$1\""; desc = "Open"; for = "linux"; }
+          {
+            run = "xdg-open \"$1\"";
+            desc = "Open";
+            for = "linux";
+          }
         ];
         reveal = [
-          { run = "xdg-open \"$(dirname \"$1\")\""; desc = "Reveal"; for = "linux"; }
-          { run = "exiftool \"$1\"; echo \"Press enter to exit\"; read _"; block = true; desc = "Show EXIF"; for = "unix"; }
+          {
+            run = "xdg-open \"$(dirname \"$1\")\"";
+            desc = "Reveal";
+            for = "linux";
+          }
+          {
+            run = "exiftool \"$1\"; echo \"Press enter to exit\"; read _";
+            block = true;
+            desc = "Show EXIF";
+            for = "unix";
+          }
         ];
         extract = [
-          { run = "ya pub extract --list \"$@\""; desc = "Extract here"; for = "unix"; }
+          {
+            run = "ya pub extract --list \"$@\"";
+            desc = "Extract here";
+            for = "unix";
+          }
         ];
         play = [
-          { run = "mpv --force-window \"$@\""; orphan = true; for = "unix"; }
-          { run = "mpv --force-window %*"; orphan = true; for = "windows"; }
-          { run = "mediainfo \"$1\"; echo \"Press enter to exit\"; read _"; block = true; desc = "Show media info"; for = "unix"; }
+          {
+            run = "mpv --force-window \"$@\"";
+            orphan = true;
+            for = "unix";
+          }
+          {
+            run = "mediainfo \"$1\"; echo \"Press enter to exit\"; read _";
+            block = true;
+            desc = "Show media info";
+            for = "unix";
+          }
         ];
       };
 
       open = {
         rules = [
-          { name = "*/"; use = [ "edit" "open" "reveal" ]; }
-          { mime = "text/*"; use = [ "edit" "reveal" ]; }
-          { mime = "image/*"; use = [ "open" "reveal" ]; }
-          { mime = "{audio,video}/*"; use = [ "play" "reveal" ]; }
-          { mime = "application/{,g}zip"; use = [ "extract" "reveal" ]; }
-          { mime = "application/x-{tar,bzip*,7z-compressed,xz,rar}"; use = [ "extract" "reveal" ]; }
-          { mime = "application/{json,x-ndjson}"; use = [ "edit" "reveal" ]; }
-          { mime = "*/javascript"; use = [ "edit" "reveal" ]; }
-          { mime = "inode/x-empty"; use = [ "edit" "reveal" ]; }
-          { name = "*"; use = [ "open" "reveal" ]; }
+          {
+            name = "*/";
+            use = ["edit" "open" "reveal"];
+          }
+          {
+            mime = "text/*";
+            use = ["edit" "reveal"];
+          }
+          {
+            mime = "image/*";
+            use = ["open" "reveal"];
+          }
+          {
+            mime = "{audio,video}/*";
+            use = ["play" "reveal"];
+          }
+          {
+            mime = "application/{,g}zip";
+            use = ["extract" "reveal"];
+          }
+          {
+            mime = "application/x-{tar,bzip*,7z-compressed,xz,rar}";
+            use = ["extract" "reveal"];
+          }
+          {
+            mime = "application/{json,x-ndjson}";
+            use = ["edit" "reveal"];
+          }
+          {
+            mime = "*/javascript";
+            use = ["edit" "reveal"];
+          }
+          {
+            mime = "inode/x-empty";
+            use = ["edit" "reveal"];
+          }
+          {
+            name = "*";
+            use = ["open" "reveal"];
+          }
         ];
       };
 
@@ -72,39 +135,8 @@
         macro_workers = 25;
         bizarre_retry = 5;
         image_alloc = 536870912;
-        image_bound = [ 0 0 ];
+        image_bound = [0 0];
         suppress_preload = false;
-      };
-
-      plugin = {
-        # when uncommented there is an error because of "if"
-        #fetchers = [
-          #{ id = "mime"; name = "*"; run = "mime"; if = "!mime"; prio = "high"; }
-        #];
-        preloaders = [
-          { mime = "image/{avif,hei?,jxl,svg+xml}"; run = "magick"; }
-          { mime = "image/*"; run = "image"; }
-          { mime = "video/*"; run = "video"; }
-          { mime = "application/pdf"; run = "pdf"; }
-          { mime = "font/*"; run = "font"; }
-          { mime = "application/vnd.ms-opentype"; run = "font"; }
-        ];
-        previewers = [
-          { name = "*/"; run = "folder"; sync = true; }
-          { mime = "text/*"; run = "code"; }
-          { mime = "*/{xml,javascript,x-wine-extension-ini}"; run = "code"; }
-          { mime = "application/{json,x-ndjson}"; run = "json"; }
-          { mime = "image/{avif,hei?,jxl,svg+xml}"; run = "magick"; }
-          { mime = "image/*"; run = "image"; }
-          { mime = "video/*"; run = "video"; }
-          { mime = "application/pdf"; run = "pdf"; }
-          { mime = "application/{,g}zip"; run = "archive"; }
-          { mime = "application/x-{tar,bzip*,7z-compressed,xz,rar,iso9660-image}"; run = "archive"; }
-          { mime = "font/*"; run = "font"; }
-          { mime = "application/vnd.ms-opentype"; run = "font"; }
-          { mime = "inode/x-empty"; run = "empty"; }
-          { name = "*"; run = "file"; }
-        ];
       };
 
       input = {
@@ -113,73 +145,73 @@
         cd = {
           title = "Change directory:";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         create = {
           title = "Create:";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         rename = {
           title = "Rename:";
           origin = "hovered";
-          offset = [ 0 1 50 3 ];
+          offset = [0 1 50 3];
         };
 
         trash = {
           title = "Move {n} selected file{s} to trash? (y/N)";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         delete = {
           title = "Delete {n} selected file{s} permanently? (y/N)";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         filter = {
           title = "Filter:";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         find = {
-          title = [ "Find next:" "Find previous:" ];
+          title = ["Find next:" "Find previous:"];
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         search = {
           title = "Search via {n}:";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         shell = {
-          title = [ "Shell:" "Shell (block):" ];
+          title = ["Shell:" "Shell (block):"];
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         overwrite = {
           title = "Overwrite an existing file? (y/N)";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         quit = {
           title = "{n} task{s} running, sure to quit? (y/N)";
           origin = "top-center";
-          offset = [ 0 2 50 3 ];
+          offset = [0 2 50 3];
         };
 
         select = {
           open_title = "Open with:";
           open_origin = "hovered";
-          open_offset = [ 0 1 50 7 ];
+          open_offset = [0 1 50 7];
         };
       };
 
