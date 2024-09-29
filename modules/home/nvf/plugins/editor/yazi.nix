fa7efefa93be54a66be391563d0ac9c1cf84a8cf
luravoid <example@example.com>
2024-09-29 02:14:10 +0200

    nvf - yazi: better config

--- modules/home/nvf/plugins/editor/yazi.nix
+++ modules/home/nvf/plugins/editor/yazi.nix
@@ -3,11 +3,13 @@
     extraPlugins = with pkgs.vimPlugins; {
       yazi-nvim = {
         package = yazi-nvim;
-        setup = "vim.keymap.set('n', '<leader>cy', function()
-          require('yazi').yazi()
-         end)";
-        # https://github.com/mikavilpas/yazi.nvim/issues/340
-        # "require('yazi').setup()";
+        setup = "require('yazi').setup{}";
+      };
+    };
+    maps.normal = {
+      "<leader>sy" = {
+        desc = "Yazi";
+        action = "<cmd>Yazi<cr>";
       };
     };
   };
