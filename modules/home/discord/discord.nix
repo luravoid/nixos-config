ca6dff3bdade3b06ac29ce2f862aed1b0301a3af
luravoid <example@example.com>
2024-10-14 06:27:10 +0200

    add vesktop

--- modules/home/discord/discord.nix
+++ modules/home/discord/discord.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: 
-{
+{pkgs, ...}: {
   home.packages = with pkgs; [
     # discord
-    (discord.override { 
-     withVencord = true; 
+    (discord.override {
+      withVencord = true;
     })
+    vesktop
   ];
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
 }
