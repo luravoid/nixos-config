63c91417774d7e2e66d97577500ae7e5a3c268a8
luravoid <example@example.com>
2024-10-14 06:26:55 +0200

    add lutris

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,31 +1,38 @@
-{ pkgs, config, inputs, ... }: 
 {
-  home.packages = with pkgs;[
+  pkgs,
+  config,
+  inputs,
+  ...
+}: {
+  home.packages = with pkgs; [
     ## Utils
     # gamemode
     # gamescope
     # winetricks
     # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
 
+    # Lutris
+    lutris
+
     ## Minecraft
     # prismlauncher
 
     ## Cli games
-    _2048-in-terminal
-    vitetris
-    nethack
-    
+    #_2048-in-terminal
+    #vitetris
+    #nethack
+
     ## Celeste
-    celeste-classic
-    celeste-classic-pm
+    #celeste-classic
+    #celeste-classic-pm
 
     ## Doom
     # gzdoom
-    crispy-doom
+    #crispy-doom
 
     ## Emulation
-    sameboy
-    snes9x
+    #sameboy
+    #snes9x
     # cemu
     # dolphin-emu
   ];
