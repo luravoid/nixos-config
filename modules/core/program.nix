914ecccf9ab0b294a948ade9eaed59e4bf39190c
luravoid <example@example.com>
2024-10-18 13:19:55 +0200

    add steam

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -1,11 +1,19 @@
-{ pkgs, lib, ... }: 
 {
+  pkgs,
+  lib,
+  ...
+}: {
   programs.dconf.enable = true;
   programs.zsh.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
     # pinentryFlavor = "";
+  };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = false;
   };
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [];
