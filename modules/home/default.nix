fb5696bf4044fe678dcb5bbf0dea2bf90c74a2d0
luravoid <example@example.com>
2024-10-21 22:50:13 +0200

    remove unused packages

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -5,32 +5,29 @@
     ./bat.nix                         # better cat command
     ./btop.nix                        # resouces monitor 
     ./cava.nix                        # audio visualizer
-    ./discord/discord.nix                     # discord with catppuccin theme
-    ./fastfetch.nix                         # fetch tool
+    ./discord/discord.nix             # discord with catppuccin theme
+    ./fastfetch.nix                   # fetch tool
     ./floorp/floorp.nix               # firefox based browser
     ./brave.nix                       # brave browser
     ./fzf.nix                         # fuzzy finder
     ./gaming.nix                      # packages related to gaming
     ./git.nix                         # version control
-    ./gnome.nix                         # gnome apps
+    ./gnome.nix                       # gnome apps
     ./gtk.nix                         # gtk theme
     ./hyprland                        # window manager
     ./kitty.nix                       # terminal
     ./foot.nix                        # terminal
     ./swaync/swaync.nix               # notification deamon
     ./nvf/nvf.nix                     # neovim config framework
-    ./micro.nix                       # nano replacement
-    ./lazyvim/nvim.nix                        # neovim editor
+    ./lazyvim/nvim.nix                # neovim editor
     ./packages.nix                    # other packages
-    ./retroarch.nix  
-    ./rofi/rofi.nix                        # launcher
+    ./rofi/rofi.nix                   # launcher
     ./scripts                         # personal scripts
     ./spicetify.nix                   # spotify client
     ./starship.nix                    # shell prompt
     ./swayimg.nix                     # image viewer 
     ./tofi.nix                        # launcher
     ./yazi                            # file manager
-    
     ./vscodium.nix                    # vscode forck
     ./waybar/waybar.nix               # status bar
     ./xdg-mimes.nix                   # xdg config
@@ -40,6 +37,8 @@
     #./aseprite/aseprite.nix           # pixel art editor
     #./audacious.nix                   # music player
     #./swaylock.nix                    # lock screen
+    #./micro.nix                       # nano replacement
+    #./retroarch.nix  
 
   ];
 }
